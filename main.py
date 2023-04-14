1976758a6277f37cf6cef01c110ef5191f83f951
fcattafesta <filippo.cattafesta99@gmail.com>
2023-04-14 15:27:17 +0200

    DY 0J

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import njets
 
 root = "/gpfs/ddn/srm/cms/store/mc/RunIISummer20UL18NanoAODv9/"
-sample = "DYJetsToLL_1J_TuneCP5_13TeV-amcatnloFXFX-pythia8"
+sample = "DYJetsToLL_0J_TuneCP5_13TeV-amcatnloFXFX-pythia8"
 aod = "/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/"
 
 path = root + sample + aod
@@ -17,4 +17,4 @@ files_paths = [os.path.join(path, f) for f in files]
 
 print(files_paths[0])
 
-njets.njet(path=files_paths[0], njets=1, var="LHE_NpNLO", outpath="DY_1J_LHE_NpNLO.png")
+njets.njet(path=files_paths[0], njets=0, var="LHE_NpNLO", outpath="DY_0J_LHE_NpNLO.png")
