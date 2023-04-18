5f8b40b011a2e37a0d14e5e74fb06b787aa28e5c
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-04-18 10:37:38 +0200

    update table

--- rdf.py
+++ rdf.py
@@ -6,6 +6,10 @@ parser.add_argument("-f", "--file",   default="", type=str,
                             help="file name" )
 args = parser.parse_args()
 
+ROOT.ROOT.EnableImplicitMT(100)
+thread_size = ROOT.ROOT.GetThreadPoolSize()
+print(">>> Thread pool size for parallel processing: %s", thread_size)
+
 file_name = f"/gpfs/ddn/srm/cms/store/mc/RunIISummer20UL18NanoAODv9/{args.file}"
 rdf = ROOT.RDataFrame("Events", file_name)
 
@@ -30,7 +34,7 @@ RVec<TLorentzVector> lepFourVec(VecF lep_pt, VecF lep_eta, VecF lep_phi, VecF le
 """)
 
 
-rdf = (
+'''rdf = (
     rdf.Define("muon_mask", "abs(LHEPart_pdgId) == 13")
     .Define("muon_pt", "LHEPart_pt[muon_mask]")
     .Define("muon_eta", "LHEPart_eta[muon_mask]")
@@ -52,20 +56,49 @@ rdf = (
     .Define("tau_mass", "LHEPart_mass[tau_mask]")
     .Define("Tau_fourvec", "lepFourVec(tau_pt, tau_eta, tau_phi, tau_mass)")
     .Define("tau_inv_mass", "(Tau_fourvec[0]+Tau_fourvec[1]).M()")
+)'''
+
+rdf= (
+    rdf.Define("lepton_mask", "abs(LHEPart_pdgId) >= 11 &&  abs(LHEPart_pdgId) <= 15")
+    .Define("lepton_pt", "LHEPart_pt[lepton_mask]")
+    .Define("lepton_eta", "LHEPart_eta[lepton_mask]")
+    .Define("lepton_phi", "LHEPart_phi[lepton_mask]")
+    .Define("lepton_mass", "LHEPart_mass[lepton_mask]")
+    .Define("Lepton_fourvec", "lepFourVec(lepton_pt, lepton_eta, lepton_phi, lepton_mass)")
+    .Define("lepton_inv_mass", "(Lepton_fourvec[0]+Lepton_fourvec[1]).M()")
 )
 
-histo_mu=rdf.Histo1D(("muon_inv_mass", "muon_inv_mass", 64, 0., 100),"muon_inv_mass")
-histo_el=rdf.Histo1D(("electron_inv_mass", "electron_inv_mass", 64, 0., 100),"electron_inv_mass")
-histo_tau=rdf.Histo1D(("tau_inv_mass", "tau_inv_mass", 64, 0., 100),"tau_inv_mass")
+rdf_empty = rdf.Filter(" LHE_Vpt<50 || LHE_Vpt > 100")
+empty_histos = rdf_empty.Histo1D(("lepton_inv_mass_empty", "lepton_inv_mass_empty", 200, -10, 200),"lepton_inv_mass")
+c=ROOT.TCanvas()
+empty_histos.Draw()
+c.SaveAs("empty.png")
+
+histo_lep=rdf.Histo1D(("lepton_inv_mass", "lepton_inv_mass", 200, 0., 200),"lepton_inv_mass")
+c2=ROOT.TCanvas()
+histo_lep.Draw()
+c2.SaveAs("inv_mass.png")
+
+
+'''histo_mu=rdf.Histo1D(("muon_inv_mass", "muon_inv_mass", 200, 0., 200),"muon_inv_mass")
+histo_el=rdf.Histo1D(("electron_inv_mass", "electron_inv_mass", 200, 0., 200),"electron_inv_mass")
+histo_tau=rdf.Histo1D(("tau_inv_mass", "tau_inv_mass", 200, 0., 200),"tau_inv_mass")
 print(type(histo_mu))
 #histo_sum=ROOT.TH1.Add(histo_mu, histo_el)
 c=ROOT.TCanvas()
 histo_el.Draw()
 histo_mu.Draw("same")
+histo_tau.Draw("same")
 c.SaveAs("inv_mass.png")
 
 
-histo_ht=rdf.Histo1D(("HT", "HT", 64, 0., 150),"LHE_HT")
+histo_ht=rdf.Histo1D(("HT", "HT", 400, 300, 700),"LHE_HT")
 c1=ROOT.TCanvas()
 histo_ht.Draw()
 c1.SaveAs("HT.png")
+'''
+
+histo_z=rdf.Histo1D(("LHE_Vpt", "LHE_Vpt", 400, -10, 110),"LHE_Vpt")
+c1=ROOT.TCanvas()
+histo_z.Draw()
+c1.SaveAs("z.png")
--- table.md
+++ table.md
@@ -1,4 +1,26 @@
-# table
+# Variables
 | Selection | Variable |
 | -------- | -------- |
 | M | invariant mass of the same signed oppite charge lepton pair |
+| HT | LHE_HT or LHE_HTIncoming (are they equivalent?)|
+| #J | LHE_NpNLO|
+| LHEFilterPtZ | LHE_Vpt |
+
+
+
+# Datasets
+| Dataset | Number of events | xsec | L equivalent |
+| -------- | -------- | -------- | -------- |
+| /DYJetsToLL_M-10to50_TuneCP5_13TeV-madgraphMLM-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM| 94452816 |
+| /DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v2/NANOAODSIM | 195510810 |
+/DYJetsToLL_M-50_HT-70to100_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM | 17004433 |
+/DYJetsToLL_M-50_HT-100to200_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM | 26202328 |
+/DYJetsToLL_M-50_HT-200to400_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM | 18455718 |
+/DYJetsToLL_M-50_HT-400to600_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM | 8682257 |
+/DYJetsToLL_M-50_HT-600to800_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM | 7035971 |
+/DYJetsToLL_M-50_HT-800to1200_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM | 6554679 |
+/DYJetsToLL_M-50_HT-1200to2500_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM | 5966661 |
+/DYJetsToLL_M-50_HT-2500toInf_TuneCP5_PSweights_13TeV-madgraphMLM-pythia8/RunIISummer20UL18NanoAODv9-106X_upgrade2018_realistic_v16_L1v1-v1/NANOAODSIM | 1978203 |
+
+
+17004433+26202328+18455718+8682257+7035971+6554679+5966661+1978203 = 9.188025×10⁷
