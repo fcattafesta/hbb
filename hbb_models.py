a545e450ea3a609434f6f10e046402c9cd3b10ba
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-04-28 17:02:18 +0200

    hbb anil

---
+++ hbb_models.py
@@ -0,0 +1,12 @@
+from samples import *
+
+name="Test"
+
+DY_Zpt={'DY':['DYZpt-0To50', 'DYZpt-50To100', 'DYZpt-100To250', 'DYZpt-250To400', 'DYZpt-400To650', 'DYZpt-650ToInf']}
+
+import ROOT
+fillcolor={
+"DY": ROOT.kBlue,
+}
+linecolor=fillcolor
+markercolor=fillcolor
---
+++ hbb_samples.py
@@ -0,0 +1,11 @@
+main_dir='/gpfs/ddn/srm/cms/store/mc/RunIISummer20UL18NanoAODv9/'
+suffix='/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/'
+sample = {
+    'DYM50' : {'folder': main_dir+'DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8'+suffix, 'xsec':5765.4},
+    'DYZpt-0To50': {'folder': main_dir+'DYJetsToLL_LHEFilterPtZ-0To50_MatchEWPDG20_TuneCP5_13TeV-amcatnloFXFX-pythia8'+suffix, 'xsec': 5028.9},
+    'DYZpt-50To100': {'folder': main_dir+'DYJetsToLL_LHEFilterPtZ-50To100_MatchEWPDG20_TuneCP5_13TeV-amcatnloFXFX-pythia8'+suffix, 'xsec': 581.4},
+    'DYZpt-100To250': {'folder': main_dir+'DYJetsToLL_LHEFilterPtZ-100To250_MatchEWPDG20_TuneCP5_13TeV-amcatnloFXFX-pythia8'+suffix, 'xsec': 148.7},
+    'DYZpt-250To400': {'folder': main_dir+'DYJetsToLL_LHEFilterPtZ-250To400_MatchEWPDG20_TuneCP5_13TeV-amcatnloFXFX-pythia8'+suffix, 'xsec': 5.6},
+    'DYZpt-400To650': {'folder': main_dir+'DYJetsToLL_LHEFilterPtZ-400To650_MatchEWPDG20_TuneCP5_13TeV-amcatnloFXFX-pythia8'+suffix, 'xsec': 0.7},
+    'DYZpt-650ToInf': {'folder': main_dir+'DYJetsToLL_LHEFilterPtZ-650ToInf_MatchEWPDG20_TuneCP5_13TeV-amcatnloFXFX-pythia8'+suffix, 'xsec': 0.1},
+}
