b247c4a36aa54f23631d8324d02e8b1091e319c8
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-05-08 15:44:23 +0200

    all ss and new args

--- args_analysis.py
+++ args_analysis.py
@@ -5,6 +5,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     "-f", "--histfolder", default="out/", help="Histograms folder for plots"
 )
-
+parser.add_argument(
+    "-m", "--model", default="", help="Model to use"
+)
 parser.print_help()
 args = parser.parse_args()
