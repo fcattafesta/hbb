61583dbe229bfa09dc81a3a4cb9618c5c256227f
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-05-08 14:50:45 +0200

    new histo

--- histograms.py
+++ histograms.py
@@ -1,7 +1,7 @@
 histosPerSelection = {
     # "": ["LHE_Zpt"],
-    "PtSelMu": ["Zmm_pt", "Zmm_mass"],
-    "PtSelEl": ["Zee_pt", "Zee_mass"],
+    "PtSelMu": ["Zmm_pt", "Zmm_mass", "CleanGenJet_hadronFlavour", "nCleanGenJet"],
+    "PtSelEl": ["Zee_pt", "Zee_mass", "CleanGenJet_hadronFlavour", "nCleanGenJet"],
     # "twoOppositeSignGenMuons": ["LHE_Zpt", "Gen_Zpt", "Gen_ZMass"],
     # "lowMass": ["LHE_Zpt", "Gen_Zpt", "Gen_ZMass"],
 }
