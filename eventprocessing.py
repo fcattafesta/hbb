16fdf10ca75a52fc2a4943f4ad99fdacb4016355
fcattafesta <filippo.cattafesta99@gmail.com>
2023-05-10 10:24:48 +0200

    Formatting

--- eventprocessing.py
+++ eventprocessing.py
@@ -10,18 +10,7 @@ def getFlow():
         "Analysis", "/scratchnvme/malucchi/1574B1FB-8C40-A24E-B059-59A80F397A0F.root"
     )
 
-    # Cleaning of GenJet collection from GenLeptons
-    flow.SubCollection(
-        "GenLepton",
-        "GenPart",
-        sel="abs(GenPart_pdgId) == 11 || abs(GenPart_pdgId) == 13 || abs(GenPart_pdgId) == 15",
-    )
-    flow.MatchDeltaR("GenLepton", "GenJet")
-    flow.SubCollection(
-        "CleanGenJet",
-        "GenJet",
-        sel="GenJet_GenLeptonDr > 0.3 || GenJet_GenLeptonIdx==-1",
-    )
+    ### Muons ###
 
     # Muon selection ID
     flow.Define("Muon_iso", "(Muon_pfRelIso04_all)")
@@ -41,7 +30,6 @@ def getFlow():
         "Nonzero(MuMu0_charge != MuMu1_charge)",
         requires=["twoMuons"],
     )
-
     flow.Selection("twoOppositeSignMuons", "OppositeSignMuMu.size() > 0")
     flow.TakePair(
         "Mu",
@@ -52,10 +40,12 @@ def getFlow():
     )
     flow.Selection("PtSelMu", "Mu0_pt > 25 && Mu1_pt > 15")
 
-    # reco Z from RECO particles
+    # Z boson from muons TODO: Maybe define Zmm only
     flow.Define("Zmm", "Mu0_p4+Mu1_p4", requires=["PtSelMu"])
     flow.Define("Zmm_pt", "Zmm.Pt()", requires=["PtSelMu"])
     flow.Define("Zmm_mass", "Zmm.M()", requires=["PtSelMu"])
+
+    ### Electrons ###
 
     # Electron selection ID
     flow.Define("Electron_iso", "(Electron_pfRelIso03_all)")
@@ -84,16 +74,16 @@ def getFlow():
     )
     flow.Selection("PtSelEl", "El0_pt > 25 && El1_pt > 17")
 
-    # reco Z from RECO particles
+    # Z boson from electrons TODO: Maybe define Zee only
     flow.Define("Zee", "El0_p4+El1_p4", requires=["PtSelEl"])
     flow.Define("Zee_pt", "Zee.Pt()", requires=["PtSelEl"])
     flow.Define("Zee_mass", "Zee.M()", requires=["PtSelEl"])
 
-    # Jet selection
+    ### Jets ###
 
+    # Jet selection
     # NOTE: JEC?
     # NOTE: FSR, PUId=0, JET_pt=30
-
     flow.MatchDeltaR("SelectedElectron", "Jet")
     flow.MatchDeltaR("SelectedMuon", "Jet")
     flow.SubCollection(
@@ -106,13 +96,8 @@ def getFlow():
         "CleanJet",
         sel="CleanJet_pt > 20. && abs(CleanJet_eta) < 2.5",
     )
-    flow.Selection("twoJets", "nSelectedJet >= 2")
-    flow.Define("SelectedJet_p4", "@p4v(SelectedJet)")
-    flow.Define("Dijets", "SelectedJet_p4[0]+SelectedJet_p4[1]", requires=["twoJets"])
-    flow.Define("Mjj", "Dijets.M()")
 
-    flow.Define("ZH_dphi", "TMath::Abs(ROOT::Math::VectorUtil::DeltaPhi(Zee, Dijets))")
-
+    # B-tagging working points
     flow.Selection(
         "LeadingJetLoose", "SelectedJet_btagDeepB[0] > 0.1241", requires=["twoJets"]
     )
@@ -126,39 +111,87 @@ def getFlow():
         "SubLeadingJetLoose", "SelectedJet_btagDeepB[1] > 0.1241", requires=["twoJets"]
     )
 
-    # signal region
+    # Jet pair selection for dijet
+    flow.Selection("twoJets", "nSelectedJet >= 2")
+    flow.Define("SelectedJet_p4", "@p4v(SelectedJet)")
+    flow.Define("Dijets", "SelectedJet_p4[0]+SelectedJet_p4[1]", requires=["twoJets"])
+    flow.Define("Mjj", "Dijets.M()")
+
+    # DeltaPhi between Z and dijet
+    flow.Define(
+        "ZH_ee_dphi", "TMath::Abs(ROOT::Math::VectorUtil::DeltaPhi(Zee, Dijets))"
+    )
+    flow.Define(
+        "ZH_mm_dphi", "TMath::Abs(ROOT::Math::VectorUtil::DeltaPhi(Zmm, Dijets))"
+    )
+
+    ### Signal regions ###
+    # Electrons
     flow.Selection(
         "SR_ee",
         "Zee_mass >= 75 && Zee_mass <= 105 && Mjj >= 90 && Mjj <= 150 && Zee_pt > 75 && LeadingJetMedium && SubLeadingJetLoose",
     )
+    # Muons
     flow.Selection(
         "SR_mm",
         "Zmm_mass >= 75 && Zmm_mass <= 105 && Mjj >= 90 && Mjj <= 150 && Zmm_pt > 75 && LeadingJetMedium && SubLeadingJetLoose",
     )
 
-    # Control regions:
-    # Z bjets
+    ### Control regions ###
+    ## Z+bjets  ##
+    # Electrons
     flow.Selection(
         "CR_Zee_bjets",
-        "Zee_mass >= 85 && Zee_mass <= 97 && (Mjj < 90 || Mjj > 150) && MET_pt < 60 && LeadingJetMedium && SubLeadingJetLoose && ZH_dphi > 2.5",
+        "Zee_mass >= 85 && Zee_mass <= 97 && (Mjj < 90 || Mjj > 150) && MET_pt < 60 && LeadingJetMedium && SubLeadingJetLoose && ZH_ee_dphi > 2.5",
+    )
+    # Muons
+    flow.Selection(
+        "CR_Zmm_bjets",
+        "Zmm_mass >= 85 && Zmm_mass <= 97 && (Mjj < 90 || Mjj > 150) && MET_pt < 60 && LeadingJetMedium && SubLeadingJetLoose && ZH_mm_dphi > 2.5",
     )
 
-    # Z light jets
+    ## Z+light jets ##
+    # Electrons
     flow.Selection(
         "CR_Zee_lightjets",
-        "Zee_mass >= 75 && Zee_mass <= 105 && Mjj >= 90 && Mjj <= 150 && !LeadingJetLoose && !SubLeadingJetLoose && ZH_dphi > 2.5",
+        "Zee_mass >= 75 && Zee_mass <= 105 && Mjj >= 90 && Mjj <= 150 && !LeadingJetLoose && !SubLeadingJetLoose && ZH_ee_dphi > 2.5",
+    )
+    # Muons
+    flow.Selection(
+        "CR_Zmm_lightjets",
+        "Zmm_mass >= 75 && Zmm_mass <= 105 && Mjj >= 90 && Mjj <= 150 && !LeadingJetLoose && !SubLeadingJetLoose && ZH_mm_dphi > 2.5",
     )
 
-    # ttbar
-
+    ## ttbar ##
+    # Electrons
     flow.Selection(
-        "CR_ttbar",
+        "CR_ee_ttbar",
         "((Zee_mass >= 10 && Zee_mass <= 75)  || Zee_mass > 120) && LeadingJetTight && SubLeadingJetLoose",
+    )
+    # Muons
+    flow.Selection(
+        "CR_mm_ttbar",
+        "((Zmm_mass >= 10 && Zmm_mass <= 75)  || Zmm_mass > 120) && LeadingJetTight && SubLeadingJetLoose",
     )
 
     flowData = copy.deepcopy(flow)
 
+    ## MonteCarlo-only definitions ##
+
     flow.CentralWeight("genWeight")  # add a central weight
+
+    # Cleaning of GenJet collection from GenLeptons
+    flow.SubCollection(
+        "GenLepton",
+        "GenPart",
+        sel="abs(GenPart_pdgId) == 11 || abs(GenPart_pdgId) == 13 || abs(GenPart_pdgId) == 15",
+    )
+    flow.MatchDeltaR("GenLepton", "GenJet")
+    flow.SubCollection(
+        "CleanGenJet",
+        "GenJet",
+        sel="GenJet_GenLeptonDr > 0.3 || GenJet_GenLeptonIdx==-1",
+    )
 
     # Defining subsamples based on flavour of the leading and subleading GenJets
     flow.Define(
