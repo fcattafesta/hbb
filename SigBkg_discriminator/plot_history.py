82f7d41b62960f6e0e87ca2a2dcfb0e6a3f46da8
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-05-19 17:56:49 +0200

    fix bug history

--- SigBkg_discriminator/plot_history.py
+++ SigBkg_discriminator/plot_history.py
@@ -11,18 +11,18 @@ def read_from_txt(file):
     # EPOCH # 14  Validation batch 97.9 %         accuracy: 0.8080      //      loss: 0.4195
 
     # get accuracy and loss and separate them between training and validation
-    with open(file, 'r') as f:
+    with open(file, "r") as f:
         lines = f.readlines()
         train_accuracy = []
         train_loss = []
         val_accuracy = []
         val_loss = []
         for line in lines:
-            if 'Training' in line:
-                train_accuracy.append(float(line.split('accuracy: ')[1].split(' ')[0]))
+            if "Training" in line:
+                train_accuracy.append(float(line.split("accuracy: ")[1].split(" ")[0]))
                 train_loss.append(float(line.split()[2]))
-            if 'Validation' in line:
-                val_accuracy.append(float(line.split('accuracy: ')[1].split(' ')[0]))
+            if "Validation" in line:
+                val_accuracy.append(float(line.split("accuracy: ")[1].split(" ")[0]))
                 val_loss.append(float(line.split()[2]))
 
     return train_accuracy, train_loss, val_accuracy, val_loss
@@ -31,30 +31,36 @@ def read_from_txt(file):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-i", "--input-file", type=str, help="path to tensorboard log file"
+        "-i", "--input-dir", type=str, help="path to tensorboard log file"
     )
     args = parser.parse_args()
 
-    train_accuracy, train_loss, val_accuracy, val_loss = read_from_txt(args.input_file)
+    train_accuracy, train_loss, val_accuracy, val_loss = read_from_txt(
+        args.input_dir + "/log.txt"
+    )
 
-    for info in ["accuracy", "loss"]:
+    infos_dict = {
+        "accuracy": {"train": train_accuracy, "val": val_accuracy},
+        "loss": {"train": train_loss, "val": val_loss},
+    }
+    for type, info in infos_dict.items():
         plt.figure(figsize=(10, 10))
         plt.plot(
-            range(len(train_accuracy)),
-            train_accuracy,
-            label="Training " + info,
+            range(len(info["train"])),
+            info["train"],
+            label="Training " + type,
             color="blue",
         )
         plt.plot(
-            range(len(val_accuracy)),
-            val_accuracy,
-            label="Validation " + info,
+            range(len(info["val"])),
+            info["val"],
+            label="Validation " + type,
             color="orange",
         )
-        plt.title("Training and validation " + info)
+        plt.title("Training and validation " + type)
 
         plt.xlabel("Epoch")
-        plt.ylabel(info.capitalize())
+        plt.ylabel(type.capitalize())
         plt.legend()
-        plt.savefig(info + ".png")
+        plt.savefig(args.input_dir + type + ".png")
         plt.show()
