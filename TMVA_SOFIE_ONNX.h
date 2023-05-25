9dca5c1f26b50ca14de64d22823d9d76a5549c06
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-05-25 12:12:54 +0200

    add libs

--- TMVA_SOFIE_ONNX.h
+++ TMVA_SOFIE_ONNX.h
@@ -2,6 +2,9 @@
 #define TMVASOFIEONNX_H
 
 #include "TMVA/Sofie.h"
+#include "TMVA/RModel.hxx"
+#include "TMVA/RModelParser_ONNX.hxx"
+#include "TMVA/SOFIEHelpers.hxx"
 
 using namespace TMVA::Experimental;
 
