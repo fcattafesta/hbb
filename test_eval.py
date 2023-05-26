814d91a3bdb8235e3468d1c6e9eb1f5de2c60838
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-05-26 15:23:28 +0200

    move include

--- test_eval.py
+++ test_eval.py
@@ -7,14 +7,16 @@ sys.path.append("../")
 from sb_discriminator.DNN_input_lists import DNN_input_variables
 from args_analysis import args
 
-def getFlowDNN(model, flow=None):
 
-    ROOT.gSystem.Load('./test_dnn.so')
+def getFlowDNN(model, flow=None):
+    ROOT.gSystem.Load("./test_dnn.so")
+    ROOT.TMVA_SOFIE_ONNX(model)
     eval_string = "sofie_functor(__slot,"
     for i in DNN_input_variables[:-1]:
         eval_string += i + ", "
     eval_string = eval_string[:-2] + ")"
 
+    ROOT.gInterpreter.Declare(f'#include "model_DNN.hxx"')
 
     # test on a RDataFrame
     rdf = ROOT.RDataFrame("Events", "~/el/Snapshots/ggZH_Snapshot.root").Range(100)
