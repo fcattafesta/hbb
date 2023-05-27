940cd880eb959a7c6152d9c12af2607c422c0c9e
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-05-27 18:35:10 +0200

    model 102

--- test_dnn.h
+++ test_dnn.h
@@ -2,8 +2,8 @@
 #define TEST_H
 
 #include <TMVA/SOFIEHelpers.hxx>
-#include "model_DNN.hxx"
+#include "model_102.hxx"
 
-auto sofie_functor = TMVA::Experimental::SofieFunctor<17, TMVA_SOFIE_model_DNN::Session>(0);
+auto sofie_functor = TMVA::Experimental::SofieFunctor<17, TMVA_SOFIE_model_102::Session>(0);
 
 #endif
