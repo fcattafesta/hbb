d4ea186c43e71a4f848be84ea9d1787ac7868b2d
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-05-31 23:21:49 +0200

    add modelsVV

---
+++ modelsVV.py
@@ -0,0 +1,49 @@
+from samples import *
+
+name = "VV"
+
+background = {
+    f"VV{flavour}": [
+        f"WWTo2L2Nu_{flavour}",
+        f"WZTo2Q2L_{flavour}",
+        f"WZTo3LNu_{flavour}",
+        f"ZZTo2L2Nu_{flavour}",
+        f"ZZTo2Q2L_{flavour}",
+        f"ZZTo4L_{flavour}",
+    ]
+    for flavour in flavourVVSplitting.keys()
+}
+
+data = {}
+
+
+signal = {}
+
+import ROOT
+
+# Color palette
+
+fillcolor = {
+    f"VV{flavour}": ROOT.kOrange + i for i, flavour in zip([0, -1], flavourVVSplitting)
+}
+
+linecolor = fillcolor  # {key: ROOT.kBlack for key in fillcolor.keys()}
+markercolor = fillcolor
+
+
+# legend sorting
+backgroundSortedForLegend = []
+backgroundSortedForLegend += [
+    x for x in background if x not in backgroundSortedForLegend
+]
+backgroundSorted = backgroundSortedForLegend
+
+signalSortedForLegend = []
+signalSortedForLegend = [z for z in signal if z not in signalSortedForLegend]
+signalSorted = signalSortedForLegend
+
+from rebinning import *
+
+systematicsToPlot = []
+systematicDetail = {}
+systematicsForDC = []
