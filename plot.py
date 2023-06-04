60562f360bbd0d5643406a6b0593cffed6e1633d
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-06-04 11:21:14 +0200

    debug 3

--- plot.py
+++ plot.py
@@ -1216,7 +1216,7 @@ def makeplot(hn, saveintegrals=True):
                     histos[hn].SetMaximum(
                         (histosum[hn].GetMaximum()) ** 2,
                     )
-                setStyle(histosum[hn], noData=True)
+                setStyle(histos[hn], noData=True)
                 c.Update()
                 histos[hn].Draw("hist")
 
