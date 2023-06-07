91f538f833385fb49b3b7d830b0fc1fa7d6a7a3c
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-06-07 09:53:32 +0200

    minor

--- sb_discriminator/roc_curve.py
+++ sb_discriminator/roc_curve.py
@@ -150,10 +150,10 @@ def plt_fts(out_dir, name, fig_handle, show):
     plt.ylim([0.0005, 1.005])
     # write text on the bottom right of the plot
     plt.text(
-        0.73,
-        0.07,
-        "$t\\bar{t}$\n$p_T > 20$ GeV",
-        fontsize=13,
+        0.7,
+        0.1,
+        "$t\\bar{t}$\n$p_T > 20 \text{GeV}$",
+        fontsize=30,
         horizontalalignment="left",
         verticalalignment="bottom",
         transform=plt.gca().transAxes,
