8b4effd5165234d29a562e04677d79dd9d824830
fcattafesta <filippo.cattafesta99@gmail.com>
2023-06-12 12:03:11 +0200

    logger

---
+++ logger.py
@@ -0,0 +1,22 @@
+import logging
+
+
+def setup_logger(log_file):
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter("%(asctime)s  - %(levelname)s - %(message)s")
+
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    return logger
