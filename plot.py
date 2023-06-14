cf49c3d6daf0d98cedb51217f467481f7b6efcff
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-06-14 17:36:24 +0200

    typo

--- plot.py
+++ plot.py
@@ -1197,8 +1197,8 @@ def makeplot(hn, saveintegrals=True):
                     writer = csv.writer(file)
                     for k, v in model.rescaleSample.items():
                         writer.writerow([k, v])
-                    writer.writerows(["btag_rescale", SignificanceSum_list[0]])
-                    writer.writerows(["SignificanceSum", SignificanceSum_list[1]])
+                    writer.writerow(["btag_rescale", SignificanceSum_list[0]])
+                    writer.writerow(["SignificanceSum", SignificanceSum_list[1]])
 
         for gr in model.signalSortedForLegend:
             h = histosSignal[hn][gr]
