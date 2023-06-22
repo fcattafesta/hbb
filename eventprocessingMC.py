4bdf4e597c0d036f1af3d8be29ce3bbd8b00587c
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-06-22 15:06:23 +0200

    multiply weights

--- eventprocessingMC.py
+++ eventprocessingMC.py
@@ -91,9 +91,9 @@ def getFlowMC(flow, btag, sf=False):
             flow.AddCppCode(
                 """template <typename VecI, typename Vec>
                     auto sf_btag(const VecI & hadronFlavour, const Vec & eta, const Vec & pt, const Vec & btag) {
-                    ROOT::VecOps::RVec<float> res(hadronFlavour.size());
-                    for(size_t i=0;i<hadronFlavour.size(); i++) res[i]=btag_shape_corr->evaluate({"central", hadronFlavour[i], abs(eta[i]), pt[i], btag[i]});
-                    return res;
+                    float weight=1.;
+                    for(size_t i=0;i<hadronFlavour.size(); i++) weight=weight*btag_shape_corr->evaluate({"central", hadronFlavour[i], abs(eta[i]), pt[i], btag[i]});
+                    return weight;
                     }"""
             )
             flow.Define(
