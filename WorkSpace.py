c933d119433abe961f9a63276def7bfecf21c0d9
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-07-03 11:53:21 +0200

    minor

--- WorkSpace.py
+++ WorkSpace.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 ##############################################################################################################
 #########################  region[x] : keys are plot names, values are region names  #########################
 ##############################################################################################################
-regionName = {"SR_mm": "SR_mm"}
+regionName = {"SR_mm": "SR_mm", "CR_Zmm_bjets":"CR_Zmm_bjets"}
 
 
 def writeSystematic(
