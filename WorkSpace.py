a9c82bca6976815519959ed42b21aa9b314a3556
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-07-05 10:26:11 +0200

    change args name 6+ rm plot_ws

--- WorkSpace.py
+++ WorkSpace.py
@@ -6,7 +6,9 @@ import copy
 import re
 import logging
 
-from histograms import selsMu, selsEle
+#from histograms import selsMu, selsEle
+selsMu = ["SR_mm", "CR_Zmm_bjets", "CR_Zmm_lightjets", "CR_mm_ttbar"]
+selsEle = ["SR_ee", "CR_Zee_bjets", "CR_Zee_lightjets", "CR_ee_ttbar"]
 
 logger = logging.getLogger(__name__)
 
--- sb_discriminator/args_train.py
+++ sb_discriminator/args_train.py
@@ -1,57 +1,57 @@
 import argparse
 
-parser = argparse.ArgumentParser()
+parser_t = argparse.ArgumentParser()
 
-parser.add_argument(
+parser_t.add_argument(
     "-b",
     "--batch-size",
     default=32,
     help="Batch size",
     type=int,
 )
-parser.add_argument(
+parser_t.add_argument(
     "-e",
     "--epochs",
     default=10,
     help="Number of epochs",
     type=int,
 )
-parser.add_argument(
+parser_t.add_argument(
     "-t",
     "--train-size",
     default=-1,
     help="Number of events to process during training",
     type=int,
 )
-parser.add_argument(
+parser_t.add_argument(
     "-v",
     "--val-size",
     default=-1,
     help="Number of events to process during validation",
     type=int,
 )
-parser.add_argument(
+parser_t.add_argument(
     "-te",
     "--test-size",
     default=-1,
     help="Number of events to process during testing",
     type=int,
 )
-parser.add_argument(
+parser_t.add_argument(
     "-p",
     "--num-prints",
     default=50,
     help="Number of prints for each epoch",
     type=int,
 )
-parser.add_argument(
+parser_t.add_argument(
     "-n",
     "--num-workers",
     default=4,
     help="Number of workers for data loading",
     type=int,
 )
-parser.add_argument(
+parser_t.add_argument(
     "-d",
     "--data-dirs",
     nargs="+",
@@ -63,66 +63,66 @@ parser.add_argument(
     ],
     help="Directory for data",
 )
-parser.add_argument(
+parser_t.add_argument(
     "--eval",
     action="store_true",
     help="Evaluate the model",
     default=False,
 )
-parser.add_argument(
+parser_t.add_argument(
     "-g", "--gpus", default="", help="GPU numbers separated by a comma", type=str
 )
-parser.add_argument(
+parser_t.add_argument(
     "--histos", default=False, help="Make histograms", action="store_true"
 )
-parser.add_argument(
+parser_t.add_argument(
     "--history", default=False, help="Plot history", action="store_true"
 )
-parser.add_argument(
+parser_t.add_argument(
     "--eval-model",
     default="",
     help="Path to model to evaluate",
     type=str,
 )
-parser.add_argument(
+parser_t.add_argument(
     "-l",
     "--load-model",
     default="",
     help="Path to model to load and continue training",
     type=str,
 )
-parser.add_argument(
+parser_t.add_argument(
     "-o",
     "--onnx",
     default=False,
     help="Save model in ONNX format",
     action="store_true",
 )
-parser.add_argument(
+parser_t.add_argument(
     "--pin-memory",
     default=False,
     help="Pin memory for data loading",
     action="store_true",
 )
-parser.add_argument(
+parser_t.add_argument(
     "--weights",
     nargs="+",
     default=[50, 0.014],  # [50, 0.01] for csv, [50, 0.014] for flav
     help="Weights for the loss function (signal, background)",
     type=float,
 )
-parser.add_argument(
+parser_t.add_argument(
     "--name",
     default="",
     help="Name for the directory",
     type=str,
 )
-parser.add_argument(
+parser_t.add_argument(
     "--noVV",
     default=False,
     help="Remove VV from the background",
     action="store_true",
 )
 
-parser.print_help()
-args = parser.parse_args()
+parser_t.print_help()
+args_t = parser_t.parse_args()
