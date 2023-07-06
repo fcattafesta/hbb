fa1601d16deb9e7e1c24ad82e574145c124397ac
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-07-06 14:08:26 +0200

    c++ in python file

--- eventprocessingSys.py
+++ eventprocessingSys.py
@@ -15,8 +15,8 @@ sf_btag = {
 
 def getFlowSys(flow, btag):
     ## Systematics definitions ##
-    #flow.AddCppCode('#include "correction.h"\n')
-    flow.AddCppCode('#include "correctionlib_sys.h"\n')
+    flow.AddCppCode('#include "correction.h"\n')
+    #flow.AddCppCode('#include "correctionlib_sys.h"\n')
 
     # btag systematics
     flow.AddCppCode(
@@ -26,47 +26,48 @@ def getFlowSys(flow, btag):
         'auto btag_shape_corr = btag_corr->at("%s");\n'
         % ("deepJet_shape" if btag == "deepflav" else "deepCSV_shape")
     )
-    # flow.AddCppCode(
-    #     """
-    #     #include <cstring>
 
-    #     // Calculate b-tagging scale factors for a given set of inputs
-    #     template <typename str, typename VecI, typename Vec>
-    #     auto sf_btag(const str & name, const VecI & hadronFlavour, const Vec & eta, const Vec & pt, const Vec & btag) {
-    #         // Create a vector to store the scale factors
-    #         ROOT::VecOps::RVec<float> sf(hadronFlavour.size());
+    flow.AddCppCode(
+        """
+        #include <cstring>
 
-    #         int flav[3];
-    #         if (strstr(name, "central") != nullptr) {
-    #             flav[0] = 0; flav[1] = 5; flav[2] = 4;
-    #         } else if (strstr(name, "hf") != nullptr || strstr(name, "lf") != nullptr) {
-    #             flav[0] = 0; flav[1] = 5; flav[2] = -1;
-    #         } else {
-    #             flav[0] = 4; flav[1] = -1; flav[2] = -1;
-    #         }
+        // Calculate b-tagging scale factors for a given set of inputs
+        template <typename str, typename VecI, typename Vec>
+        auto sf_btag(const str & name, const VecI & hadronFlavour, const Vec & eta, const Vec & pt, const Vec & btag) {
+            // Create a vector to store the scale factors
+            ROOT::VecOps::RVec<float> sf(hadronFlavour.size());
 
-    #         // Loop over each input and calculate the scale factor
-    #         for(size_t i=0;i<hadronFlavour.size(); i++) {
-    #             bool correct_flav = false;
-    #             // Loop over each flavor and check if it matches the input flavor
-    #             for (long unsigned int j = 0; j < sizeof(flav) / sizeof(flav[0]); j++) {
-    #                 if (hadronFlavour[i] == flav[j]) {
-    #                     // Calculate the scale factor using the btag_shape_corr object
-    #                     sf[i]=btag_shape_corr->evaluate({name, hadronFlavour[i], abs(eta[i]), pt[i], btag[i]});
-    #                     correct_flav = true;
-    #                     break;
-    #                 }
-    #             }
-    #             // If no matching flavor is found, set the scale factor to 1
-    #             if (!correct_flav) {
-    #                 sf[i]=1.;
-    #             }
-    #         }
-    #         // Return the vector of scale factors
-    #         return sf;
-    #     }
-    # """
-    # )
+            int flav[3];
+            if (strstr(name, "central") != nullptr) {
+                flav[0] = 0; flav[1] = 5; flav[2] = 4;
+            } else if (strstr(name, "hf") != nullptr || strstr(name, "lf") != nullptr) {
+                flav[0] = 0; flav[1] = 5; flav[2] = -1;
+            } else {
+                flav[0] = 4; flav[1] = -1; flav[2] = -1;
+            }
+
+            // Loop over each input and calculate the scale factor
+            for(size_t i=0;i<hadronFlavour.size(); i++) {
+                bool correct_flav = false;
+                // Loop over each flavor and check if it matches the input flavor
+                for (long unsigned int j = 0; j < sizeof(flav) / sizeof(flav[0]); j++) {
+                    if (hadronFlavour[i] == flav[j]) {
+                        // Calculate the scale factor using the btag_shape_corr object
+                        sf[i]=btag_shape_corr->evaluate({name, hadronFlavour[i], abs(eta[i]), pt[i], btag[i]});
+                        correct_flav = true;
+                        break;
+                    }
+                }
+                // If no matching flavor is found, set the scale factor to 1
+                if (!correct_flav) {
+                    sf[i]=1.;
+                }
+            }
+            // Return the vector of scale factors
+            return sf;
+        }
+    """
+    )
 
     for suffix, names in sf_btag.items():
         for i, name in enumerate(names):
@@ -99,25 +100,24 @@ def getFlowSys(flow, btag):
         % "Summer19UL18_JRV2_MC_ScaleFactor_AK4PFchs"
     )
 
-    # flow.AddCppCode(
-    #     """
+    flow.AddCppCode(
+        """
+        // Calculate jer scale factors for a given set of inputs
+        template <typename Vec, typename str>
+        auto sf_jer(const str & name, const Vec & eta) {
+            // Create a vector to store the scale factors
+            ROOT::VecOps::RVec<float> sf(eta.size());
 
-    #     // Calculate jer scale factors for a given set of inputs
-    #     template <typename Vec, typename str>
-    #     auto sf_jer(const str & name, const Vec & eta) {
-    #         // Create a vector to store the scale factors
-    #         ROOT::VecOps::RVec<float> sf(eta.size());
-
-    #         // Loop over each input and calculate the scale factor
-    #         for(size_t i=0;i<eta.size(); i++) {
-    #             // Calculate the scale factor using the jer_sf_corr object
-    #             sf[i]=jer_sf_corr->evaluate({eta[i], name});
-    #         }
-    #         // Return the vector of scale factors
-    #         return sf;
-    #     }
-    # """
-    # )
+            // Loop over each input and calculate the scale factor
+            for(size_t i=0;i<eta.size(); i++) {
+                // Calculate the scale factor using the jer_sf_corr object
+                sf[i]=jer_sf_corr->evaluate({eta[i], name});
+            }
+            // Return the vector of scale factors
+            return sf;
+        }
+    """
+    )
 
     flow.Define("Jet_genPt", "TakeDef(GenJet_pt,Jet_genJetIdx,Jet_pt)")
     for name in ["Nom", "Up", "Down"]:
