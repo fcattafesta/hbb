91ea7a7e5fbd287e8daefded7d5e52d7faed2fa1
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-07-11 11:14:44 +0200

    typo

--- dijet_mass.py
+++ dijet_mass.py
@@ -5,7 +5,7 @@ from array import array
 # Define a dictionary to store the ROOT files
 root_files = {
     "Jet_pt":["/gpfs/ddn/cms/user/malucchi/hbb_out/mu/deepflav_testZH/ZH_Histos.root", "Dijets_mass___SR_mm"],
-    "Jet_ptNom":["/gpfs/ddn/cms/user/malucchi/hbb_out/mu/deepflav_testZH_sf/ZH_Histos.root", "Dijets_mass___SR_mm"],
+    "Jet_pt_Nom":["/gpfs/ddn/cms/user/malucchi/hbb_out/mu/deepflav_testZH_sf/ZH_Histos.root", "Dijets_mass___SR_mm"],
     "Jet_pt_jerUp"  :["/gpfs/ddn/cms/user/malucchi/hbb_out/mu/deepflav_testZH_sf/ZH_Histos.root", "Dijets_mass__syst__JERUp___SR_mm__syst__JERUp"],
     "Jet_pt_jerDown":["/gpfs/ddn/cms/user/malucchi/hbb_out/mu/deepflav_testZH_sf/ZH_Histos.root", "Dijets_mass__syst__JERDown___SR_mm__syst__JERDown"],
 }
@@ -13,22 +13,22 @@ root_files = {
 histos={}
 histos_pt={}
 
-pts=["GenJet_pt___SR_mm", "Jet_pt___SR_mm", "Jet_ptNom___SR_mm", "Jet_pt_jerUp___SR_mm", "Jet_pt_jerDown___SR_mm"]
+pts=["GenJet_pt___SR_mm", "Jet_pt___SR_mm", "Jet_pt_Nom___SR_mm", "Jet_pt_jerUp___SR_mm", "Jet_pt_jerDown___SR_mm"]
 rebinning=list(np.linspace(50, 200, 100))
 files={}
 # Loop over the file names and add them to the dictionary
 for type, names in root_files.items():
     files[type] = ROOT.TFile.Open(names[0])
     histos[type] = files[type].Get(names[1]).Rebin(len(rebinning)-1, "hnew"+type, array("d", rebinning))
-    if type=="Jet_ptNom":
+    if type=="Jet_pt_Nom":
         for pt in pts:
             histos_pt[pt] = files[type].Get(pt)#.Rebin(len(rebinning)-1, "hnew"+pt, array("d", rebinning))
 
 print(histos_pt)
+colors=[ROOT.kBlack, ROOT.kBlue, ROOT.kRed, ROOT.kGreen, ROOT.kOrange]
 
 c=ROOT.TCanvas()
 legend=ROOT.TLegend()
-colors=[ROOT.kBlack, ROOT.kBlue, ROOT.kRed, ROOT.kGreen, ROOT.kOrange]
 for i, type in enumerate(histos):
     h=histos[type]
     h.SetLineColor(colors[i])
