0559643c475c59e75ea6d38bcf0249c4bf6cbfcb
fcattafesta <filippo.cattafesta99@gmail.com>
2023-07-14 18:04:36 +0200

    Debug selection

--- eventprocessingElectrons.py
+++ eventprocessingElectrons.py
@@ -69,20 +69,19 @@ def getFlowElectrons(flow):
         requires=["CommonSelEle"],
     )
 
-    flow.Selection("twoGenJets", "GenJet_pt.size() >= 2")
     flow.SubCollection(
         "GenLepton",
         "GenPart",
         sel="abs(GenPart_pdgId) == 11 || abs(GenPart_pdgId) == 13",
     )
     flow.MatchDeltaR("SelectedGenJet", "GenLepton")
-
     flow.SubCollection(
         "CleanedGenJet",
         "SelectedGenJet",
         sel="SelectedGenJet_GenLeptonDr > 0.4 || SelectedGenJet_GenLeptonIdx==-1",
     )
     flow.Define("CleanedGenJet_ptOrderIdx", "Argsort(-CleanedGenJet_pt)")
+    flow.Selection("twoGenJets", "nCleanedGenJet >= 2")
     flow.ObjectAt(
         "SubLeadingGenJet",
         "CleanedGenJet",
