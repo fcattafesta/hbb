dd6bab5e8a51c9b899ab4a6b4537f40ff7a684d5
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-07-15 17:52:53 +0200

    debug

--- plot.py
+++ plot.py
@@ -1175,8 +1175,8 @@ if __name__ == "__main__":
             ratio_sys.Add(histo, -1)
             setStyle(ratio_sys, isSys=True)
             ratio_sys.SetFillStyle(0)
-            ratio_sys.SetAxisRange(-0.6, 0.6, "Y")
-            ratio_sys.GetYaxis().SetNdivisions(5)
+            #ratio_sys.SetAxisRange(-0.6, 0.6, "Y")
+            #ratio_sys.GetYaxis().SetNdivisions(5)
             ratio_sys.Draw("hist")
             ratio_sys_list = []
             for i, sy in enumerate(
