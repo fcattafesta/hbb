4bacdf13fcf09750287d51d13bea35f14a8c4e82
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-07-20 09:52:14 +0200

    rm test_dnn and test_eval bc now works properly

--- test_dnn.C
+++
@@ -1,8 +0,0 @@
-//#include "test_dnn.h"
-#include "TMVA_SOFIE_ONNX.h"
-
-#include "model_DNN.hxx"
-
-#include <TMVA/SOFIEHelpers.hxx>
-
-auto sofie_functor = TMVA::Experimental::SofieFunctor<17, TMVA_SOFIE_model_DNN::Session>(0);
--- test_dnn.h
+++
@@ -1,9 +0,0 @@
-#ifndef TEST_H
-#define TEST_H
-
-#include <TMVA/SOFIEHelpers.hxx>
-#include "model_102.hxx"
-
-auto sofie_functor = TMVA::Experimental::SofieFunctor<17, TMVA_SOFIE_model_102::Session>(0);
-
-#endif
