f339a5a3645918ecd124b2b57c9f62fba7901611
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-07-22 12:44:56 +0200

    debug

--- fit.py
+++ fit.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # Plot
-parser.add_argument("-d", "--dir", help="Directory", type=str)
+parser.add_argument("-d", "--dir", help="Directory", type=str, default="workspace")
 args = parser.parse_args()
 
 workdir = f"{args.dir}/workspace/"
@@ -15,18 +15,20 @@ datacard = os.path.splitext(
 
 name = datacard.split("2018")[1]
 
-os.system(f"text2workspace.py {datacard}.txt -m 125")
+os.system(f"text2workspace.py {workdir}{datacard}.txt -m 125")
 
 os.system(
-    f"combineTool.py -M Impacts -d {datacard}.root -m 125 --doInitialFit --robustFit 1 -t -1"
+    f"combineTool.py -M Impacts -d {workdir}{datacard}.root -m 125 --doInitialFit --robustFit 1 -t -1"
 )
 os.system(
-    f"combineTool.py -M Impacts -d {datacard}.root -m 125 --robustFit 1 --doFits --parallel 30 -t -1"
+    f"combineTool.py -M Impacts -d {workdir}{datacard}.root -m 125 --robustFit 1 --doFits --parallel 30 -t -1"
 )
-os.system(f"combineTool.py -M Impacts -d {datacard}.root -m 125 -o impacts_{name}.json -t -1")
-os.system(f"plotImpacts.py -i impacts_{name}.json -o impacts_{name}")
+os.system(
+    f"combineTool.py -M Impacts -d {workdir}{datacard}.root -m 125 -o {workdir}impacts_{name}.json -t -1"
+)
+os.system(f"plotImpacts.py -i {workdir}impacts_{name}.json -o {workdir}impacts_{name}")
 
-os.system(f"mv impacts_{name}.* {workdir}/")
+#os.system(f"mv impacts_{name}.* {workdir}/")
 
 
-os.system(f"combine -M Significance {datacard}.txt -t -1 --expectSignal=1")
+os.system(f"combine -M Significance {workdir}{datacard}.txt -t -1 --expectSignal=1")
