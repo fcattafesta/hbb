6f08ef93df48532c1d3aefb6bd8a8bda4e73e1db
Matteo Malucchi <matteomalucchi13@gmail.com>
2023-07-23 12:58:05 +0200

    typo

--- histobinning.py
+++ histobinning.py
@@ -9,7 +9,7 @@ binningRules = [
     (".*_deta", "30, 0, 5"),
     (".*_dr", "30, 0, 10"),
     (".*_ptRatio", "30, 0, 2"),
-    ("btag.*", "30, 0, 1") if not args.btag_bin else ("btag.*", "4, -0.5, 3.5"),
+    ("btag.*", "30, 0, 1") if not args.btag_bit else ("btag.*", "4, -0.5, 3.5"),
     ("SoftActivityJetNjets5", "12, -0.5, 11.5"),
     ("DNN_Score", "10000, 0, 1"),
     ("atanhDNN_Score", "10000, 0, 15"),
