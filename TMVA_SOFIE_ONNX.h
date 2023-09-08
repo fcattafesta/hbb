1d9d79d592097435e4a7e71d7193c66c08db21a7
fcattafesta <filippo.cattafesta99@gmail.com>
2023-09-08 11:21:11 +0200

    TMVA print removed

--- TMVA_SOFIE_ONNX.h
+++ TMVA_SOFIE_ONNX.h
@@ -6,33 +6,33 @@
 
 using namespace TMVA::Experimental;
 
-void TMVA_SOFIE_ONNX(std::string inputFile = "")
-{
-    // Creating parser object to parse ONNX files
-    SOFIE::RModelParser_ONNX parser;
-    SOFIE::RModel model = parser.Parse(inputFile);
+void TMVA_SOFIE_ONNX(std::string inputFile = "") {
+  // Creating parser object to parse ONNX files
+  SOFIE::RModelParser_ONNX parser;
+  SOFIE::RModel model = parser.Parse(inputFile);
 
-    // Generating inference code
-    model.Generate();
+  // Generating inference code
+  model.Generate();
 
-    std::string outputFile = inputFile.substr(0, inputFile.find_last_of('.')) + ".hxx";
+  std::string outputFile =
+      inputFile.substr(0, inputFile.find_last_of('.')) + ".hxx";
 
-    model.OutputGenerated(outputFile);
+  model.OutputGenerated(outputFile);
 
-    // Printing required input tensors
-    model.PrintRequiredInputTensors();
+  // Printing required input tensors
+  // model.PrintRequiredInputTensors();
 
-    // Printing initialized tensors (weights)
-    std::cout << "\n\n";
-    model.PrintInitializedTensors();
+  // Printing initialized tensors (weights)
+  // std::cout << "\n\n";
+  // model.PrintInitializedTensors();
 
-    // Printing intermediate tensors
-    std::cout << "\n\n";
-    model.PrintIntermediateTensors();
+  // Printing intermediate tensors
+  // std::cout << "\n\n";
+  // model.PrintIntermediateTensors();
 
-    // Printing generated inference code
-    std::cout << "\n\n";
-    model.PrintGenerated();
+  // Printing generated inference code
+  // std::cout << "\n\n";
+  // model.PrintGenerated();
 }
 
 #endif
