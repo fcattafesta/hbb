3487414b617e018383ea45e5f7c56438db37416c
fcattafesta <filippo.cattafesta99@gmail.com>
2023-09-15 15:58:08 +0200

    Oversampling debug

--- samples.py
+++ samples.py
@@ -251,11 +251,11 @@ for sample in samples:
                 for x in glob.glob(
                     "/scratchnvme/cattafe/FlashSim-Oversampled/RunIISummer20UL18NanoAODv9/DYJetsToLL_LHEFilterPtZ-100To250_MatchEWPDG20_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/**/*.root"
                 )
-            ][:1]
+            ][:10]
             # samples[sample]["files"] = [
             #     "/scratchnvme/cattafe/FlashSim/RunIISummer20UL18NanoAODv9/DYJetsToLL_LHEFilterPtZ-100To250_MatchEWPDG20_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v1/2530000/B24D7292-7CA2-804F-9082-BCFDC95CFDC5.root"
             # ]
-            print(samples[sample]["files"])
+            # print(samples[sample]["files"])
             # [10 * (i) :: 10 * (i + 1)]
             # i += 1
         else:
