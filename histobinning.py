7bccc199dbcaf92519dd75ce3bd8d6be39403a8c
fcattafesta <filippo.cattafesta99@gmail.com>
2023-09-18 10:08:48 +0200

    histobinning

--- histobinning.py
+++ histobinning.py
@@ -1,20 +1,20 @@
-from btagging_sys import btag_sys
-
 binningRules = [
-    (".*_pt", "60 , 0, 300"),
+    (".*_pt", "30 , 0, 300"),
     ("Dijets_mass", "3000, 50, 200"),
-    ("Z.*_mass", "20, 10, 150"),
+    ("Z.*_mass", "3000, 10, 150"),
     (".*_dphi", "30, 0, 3.2"),
     (".*_deta", "30, 0, 5"),
     (".*_dr", "30, 0, 10"),
     (".*_ptRatio", "30, 0, 2"),
-    ("btag.*", "30, 0, 1"),
+    ("btag.*", "30, 0, 1") if not args.btag_bit else ("btag.*", "4, -0.5, 3.5"),
     ("SoftActivityJetNjets5", "12, -0.5, 11.5"),
     ("DNN_Score", "10000, 0, 1"),
     ("atanhDNN_Score", "10000, 0, 15"),
     ("hadronFlavour_btag.*", "6, -0.5, 5.5"),
     ("LHE_Nb", "6, -0.5, 5.5"),
-    ("btagWeight.*", "100, 0, 10"),
+    (".*btagWeight.*", "100, 0, 10"),
+    ("number_of_events", "1, 0.5, 1.5"),
+    ("SelectedJet_btagDeep.*B", "100, 0,1"),
 ]
 binningRules += [(".*_pt_.*", "30, 0, 300")]
 binningRules += [
