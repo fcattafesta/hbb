2b224adac39c20165b1c1ffa2d149d820effd2dc
fcattafesta <filippo.cattafesta99@gmail.com>
2023-09-18 10:39:12 +0200

    nailOriginal

--- preliminar_lhe/massinvleptHbb.py
+++ preliminar_lhe/massinvleptHbb.py
@@ -1,13 +1,22 @@
 import os
-from nail import *
+from args_analysis import args
+
+if args.oversampling:
+    from nail.nail import *
+else:
+    from nail.nailOriginal import *
 import ROOT
 import sys
 
 
 flow = SampleProcessing(
-    "Inv_Mass", "/gpfs/ddn/srm/cms/store/mc/RunIISummer20UL18NanoAODv9/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v2/250000/A87275E3-6D57-6A46-8EB2-17F616321665.root")
+    "Inv_Mass",
+    "/gpfs/ddn/srm/cms/store/mc/RunIISummer20UL18NanoAODv9/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v2/250000/A87275E3-6D57-6A46-8EB2-17F616321665.root",
+)
 
-flow.SubCollection("LHELept", "LHEPart", "abs(LHEPart_pdgId)==13 || abs(LHEPart_pdgId)==11")
+flow.SubCollection(
+    "LHELept", "LHEPart", "abs(LHEPart_pdgId)==13 || abs(LHEPart_pdgId)==11"
+)
 flow.Define("LHELept_p4", "@p4v(LHELept)")
 flow.Distinct("LHELeptLHELept", "LHELept")
 flow.Define("Lept_invMass", "MemberMap(LHELeptLHELept0_p4+LHELeptLHELept1_p4,M())")
@@ -16,10 +25,11 @@ nthreads = 10
 histos = {}
 targets = ["Lept_invMass"]
 
-processor = flow.CreateProcessor(
-    "eventProcessor", targets, histos, [], "", nthreads)
+processor = flow.CreateProcessor("eventProcessor", targets, histos, [], "", nthreads)
 rdf = ROOT.RDataFrame(
-    "Events", "/gpfs/ddn/srm/cms/store/mc/RunIISummer20UL18NanoAODv9/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v2/250000/A87275E3-6D57-6A46-8EB2-17F616321665.root")
+    "Events",
+    "/gpfs/ddn/srm/cms/store/mc/RunIISummer20UL18NanoAODv9/DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8/NANOAODSIM/106X_upgrade2018_realistic_v16_L1v1-v2/250000/A87275E3-6D57-6A46-8EB2-17F616321665.root",
+)
 
 result = processor(rdf)
 
