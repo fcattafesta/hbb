79099859e271449da13da96217cdce064daaf59d
fcattafesta <filippo.cattafesta99@gmail.com>
2023-09-18 10:49:45 +0200

    nailOriginal

--- selections.py
+++ selections.py
@@ -1,3 +1,3 @@
 ### List of selections for muons and electrons ###
-selsMu = ["SR_mm", "CR_Zmm_bjets", "CR_Zmm_lightjets", "CR_mm_ttbar"]
-selsEle = ["SR_ee", "CR_Zee_bjets", "CR_Zee_lightjets", "CR_ee_ttbar"]
+selsMu = ["SR_mm"]  # , "CR_Zmm_bjets", "CR_Zmm_lightjets", "CR_mm_ttbar"]
+selsEle = ["SR_ee"]  # , "CR_Zee_bjets", "CR_Zee_lightjets", "CR_ee_ttbar"]
