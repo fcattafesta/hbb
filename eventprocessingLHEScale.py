a86795a1c330d77fb5f8c0d8dc34c39061147808
fcattafesta <filippo.cattafesta99@gmail.com>
2023-09-27 17:30:08 +0200

    Muon and Electron study

--- eventprocessingLHEScale.py
+++ eventprocessingLHEScale.py
@@ -7,12 +7,14 @@ else:
 
 
 def getFlowLHEScale1(flow):
-    flow.CentralWeight("LHEScaleWeight[1]")  # add a central weight
+    flow.Define("LHEScaleWeight1", "LHEScaleWeight[1]")
+    flow.CentralWeight("LHEScaleWeight1")  # add a central weight
 
     return flow
 
 
 def getFlowLHEScale7(flow):
-    flow.CentralWeight("LHEScaleWeight[7]")  # add a central weight
+    flow.Define("LHEScaleWeight7", "LHEScaleWeight[7]")
+    flow.CentralWeight("LHEScaleWeight7")  # add a central weight
 
     return flow
