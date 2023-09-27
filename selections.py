230eb2d99ae3356a3ce4e62e17c4f9df72156221
fcattafesta <filippo.cattafesta99@gmail.com>
2023-09-27 15:47:17 +0200

    Muon and Electron study

--- selections.py
+++ selections.py
@@ -1,10 +1,9 @@
 ### List of selections for muons and electrons ###
-selsMu = [
-    "",
-    "twoMuons",
-    # "twoOppositeSignMuons",
-    # "PtSelMu",
-    # "CommonSelMu",
-    # "SR_mm",
-]  # , "CR_Zmm_bjets", "CR_Zmm_lightjets", "CR_mm_ttbar"]
+selsMu = ["SR_mm", "CR_Zmm_bjets", "CR_Zmm_lightjets", "CR_mm_ttbar"]
+# "twoMuons",
+# "twoOppositeSignMuons",
+# "PtSelMu",
+# "CommonSelMu",
+# "SR_mm",
+# ]  , "CR_Zmm_bjets", "CR_Zmm_lightjets", "CR_mm_ttbar"]
 selsEle = ["SR_ee"]  # , "CR_Zee_bjets", "CR_Zee_lightjets", "CR_ee_ttbar"]
